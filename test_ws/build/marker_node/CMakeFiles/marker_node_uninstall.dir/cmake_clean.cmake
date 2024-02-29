4cbfbf65b0fe535979505d1a30308a1fc6a04f2a
Luthov <lukettl15@gmail.com>
2024-03-01 00:07:18 +0800

    added marker_node

---
+++ test_ws/build/marker_node/CMakeFiles/marker_node_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/marker_node_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/marker_node_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
